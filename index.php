b74a6c5145f9fde445e7b8c349dfd007472739f0
李奕玮 <55435836+ztyfeibi@users.noreply.github.com>
2020-11-29 13:06:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include 'lib.php';
+//$servername="127.0.0.1";
+//$username="liyiwei";
+//$password="li130214";
+$conn=new mysqli(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+if ($conn->connect_error){
+    $msg="数据库连接错误";
+}//echo "mysql连接成功"."<br>";
+
+
+$name=$_GET['name'];
+$subject=$_GET['subject'];
+$tel=$_GET['tel'];
+$context=$_GET['context'];
+if (isset($name)&&isset($subject)&&isset($tel)&&isset($context) &&strlen($tel)==11){
+    $sql="INSERT INTO `register` (`name`, `subject`, `tel`, `context`) VALUES ('".$name."', '".$subject."', '".$tel."', '".$context."')";
+    if ($conn->query($sql)===TRUE){
+        $msg='报名成功';
+        //echo "insert successfully";
+    }else{
+        $msg='报名失败';
+        //die('Error:'.$sql.'<br>'.$conn->error   );
+    }
+}elseif(strlen($tel)!=11){
+    $msg='电话号码位数不正确';
+}else{
+    $msg='请填写完整信息';
+}
+$conn->close();
+echo $msg;
+?>
---
+++ lib.php
@@ -0,0 +1,6 @@
+<?php
+define('SERVERNAME','127.0.0.1');
+define('USERNAME','root');
+define('PASSWORD','NIUBSky3!.comr720');
+define('DBNAME','warmth');
+?>
