6bbca68ac6063370f4d09f0bd8db5d3e0864d295
李奕玮 <55435836+ztyfeibi@users.noreply.github.com>
2020-11-29 09:17:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <?php
 include 'lib.php';
 header("Access-Control-Allow-Origin: *");
-$servername="127.0.0.1";
-$username="liyiwei";
-$password="li130214";
-$conn=new mysqli($servername,$username,$password,'homework');
-//$conn=new mysqli(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+//$servername="127.0.0.1";
+//$username="liyiwei";
+//$password="li130214";
+//$conn=new mysqli($servername,$username,$password,'homework');
+$conn=new mysqli(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 if ($conn->connect_error){
     $msg="数据库连接错误";
 }//echo "mysql连接成功"."<br>";
 
 
 $name=$_GET['name'];
-$qq=$_GET['subject'];
+$qq=$_GET['subject'];//qq==subject
 $tel=$_GET['tel'];
 $context=$_GET['context'];
 $msg="第一步";
 
-$sql2="SELECT * FROM `warmth` WHERE `name`='".$name."'";
-$res=$conn->query($sql2);
+$sql="SELECT * FROM `register` WHERE `name`='".$name."'";
+$res=$conn->query($sql);
+//$sql=$conn->prepare("SELECT * FROM `warmth` WHERE `name`= ?");
+//$sql->bind_param("s",$name);
+//$res=$sql->execute();
 if($res->num_rows>0){
     $msg ="已报名";
     die($msg);
 }
 
 if (isset($name)&&isset($qq)&&isset($tel)&&strlen($tel)==11){
-    $sql="INSERT INTO `warmth` (`name`, `subject`, `tel`, `context`) VALUES ('".$name."', '".$qq."', '".$tel."', '".$context."')";
-    if ($conn->query($sql)===TRUE){
+    //$sql="INSERT INTO `warmth` (`name`, `subject`, `tel`, `context`) VALUES ('".$name."', '".$qq."', '".$tel."', '".$context."')";
+    $sql=$conn->prepare("INSERT INTO `register` (`name`, `subject`, `tel`, `context`) VALUES (?,?,?,?)");
+    $sql->bind_param("ssss",$name,$qq,$tel,$context);
+    $res=$sql->execute();
+    if ($res===TRUE){
         $msg='报名成功';
         //echo "insert successfully";
     }else{
@@ -42,6 +48,7 @@ if (isset($name)&&isset($qq)&&isset($tel)&&strlen($tel)==11){
 //while($row = $res->fetch_assoc()) {
 //    echo $row["name"]."<br>".  $row["subject"] ."<br>". $row["tel"] ."<br>". $row["context"]."<br>";
 //}
+$res->close();
 $conn->close();
 echo $msg;
 ?>
